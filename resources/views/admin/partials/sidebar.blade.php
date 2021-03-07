23f5e6721d08e31221fd60134cb43696a88e9f72
rohsyl <syzin12@gmail.com>
2021-03-07 10:35:27 +0100

    login

--- resources/views/admin/partials/sidebar.blade.php
+++ resources/views/admin/partials/sidebar.blade.php
@@ -12,7 +12,7 @@
 								</div>
 							</div>
 							<div class="peer peer-greed">
-								<h5 class="lh-1 mB-0 logo-text">Laradminator</h5>
+								<h5 class="lh-1 mB-0 logo-text">OmegaCMS</h5>
 							</div>
 						</div>
 					</a>
