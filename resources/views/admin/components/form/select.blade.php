1a9302ea5a98ab8b9ad37618893c911803064390
Huser <huser.gaetan@hotmail.com>
2021-04-26 15:03:38 +0200

    oselect -> selected placeholder if null colors and variables

--- resources/views/admin/components/form/select.blade.php
+++ resources/views/admin/components/form/select.blade.php
@@ -4,10 +4,10 @@
     @endif
     <select id="{{ $name }}" name="{{ $name }}"
             class="{{ isset($errors) && $errors->has($name) ? 'is-invalid' : '' }} {{ isset($attributes['class']) ? $attributes['class'] : 'form-control' }}">
-        <option
+        <option disabled {{ ($value != null) ? 'value="' . $value . '"' : 'selected'   }}
             data-placeholder="true">{{ isset($attributes['placeholder']) ? $attributes['placeholder'] : '' }}</option>
         @foreach($data as $key=>$line)
-            @if ($value == $key)
+            @if ($value == $key && (value != null))
                 <option selected value="{{ $key }}">{{ $line }}</option>
             @else
                 <option class="" value="{{ $key }}">{{ $line }}</option>
