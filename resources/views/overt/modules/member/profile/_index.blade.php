3a2839af312fe434202313208ccbeca8de5035d0
rohsyl <syzin12@gmail.com>
2021-04-30 09:48:42 +0200

    member profile page

--- resources/views/overt/modules/member/profile/_index.blade.php
+++ resources/views/overt/modules/member/profile/_index.blade.php
@@ -1,3 +1,4 @@
-<p>
-    Welcome {{ auth('member')->user()->username }}
-</p>
+<x-oix-card title="{{ __('Profile') }}" subtitle="{{ __('Informations about you') }}">
+    {{ Form::oattribute(__('Username'), auth('member')->user()->username) }}
+    {{ Form::oattribute(__('E-Mail'), auth('member')->user()->email) }}
+</x-oix-card>
